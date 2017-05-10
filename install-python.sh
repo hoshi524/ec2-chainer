9ece6f2693914ca031c8d27f42e120d65e91a83d
hoshimamoru <hoshimamoru@team-lab.com>
2017-05-10 16:44:57 +0900

    install python

---
+++ install-python.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+set -e
+
+cd
+
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+sh  ~/.bashrc
+
+pyenv install 3.6.1
+pyenv global 3.6.1
