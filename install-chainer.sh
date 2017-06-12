07b986043a16ee4c26cebd4c7d7b6391dbfed05a
hoshimamoru <hoshimamoru@team-lab.com>
2017-06-12 21:14:46 +0900

    update

--- install-chainer.sh
+++ install-chainer.sh
@@ -6,12 +6,16 @@ cd
 
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo dpkg -i cuda-repo-*_amd64.deb
-sudo apt update
-sudo apt -y install cuda
+sudo apt-get update
+sudo apt-get -y install cuda
 
-echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
-. ~/.bashrc
+setting() {
+    eval $1
+    echo $1 >> ~/.bashrc
+}
+
+setting 'export PATH=/usr/local/cuda/bin:$PATH'
+setting 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH'
 
 pip install -U setuptools
 pip install cupy
--- install-python.sh
+++ install-python.sh
@@ -2,16 +2,21 @@
 set -e
 cd
 
+sudo apt-get update
 sudo apt-get -y install git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
 
 # https://github.com/pyenv/pyenv
 
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 
-echo 'export PATH=$HOME/.pyenv/bin:$PATH' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-. ~/.bashrc
+setting() {
+    eval $1
+    echo $1 >> ~/.bashrc
+}
 
-pyenv install 3.6.1
-pyenv global 3.6.1
+setting 'export PATH=$HOME/.pyenv/bin:$PATH'
+setting 'eval "$(pyenv init -)"'
+setting 'eval "$(pyenv virtualenv-init -)"'
+
+pyenv install anaconda3-4.4.0
+pyenv global anaconda3-4.4.0
